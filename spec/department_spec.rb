95562b67368836e4f0beb73529e6d63e5bd2698b
Luke Pascale <lpluke5@gmail.com>
2022-01-12 11:16:44 -0700

    feat: Add expenses value to department file.

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -30,5 +30,15 @@ RSpec.describe Department do
 
       expect(customer_service.employees).to eq([bobbi, aaron])
     end
+
+    it 'can add expenses' do
+      customer_service = Department.new("Customer Service")
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+      aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+      expect(customer_service.expenses).to be(0)
+    end
   end
 end
