679470e42851a8087f52317bce3e428e9d003c80
Luke Pascale <lpluke5@gmail.com>
2022-01-19 13:44:37 -0700

    feat: Add ability to read departments with multiple employees.

--- lib/division.rb
+++ lib/division.rb
@@ -1,6 +1,23 @@
 class Division
-  attr_reader :name 
+  attr_reader :name, :departments, :deps_with_multiple
   def initialize(name)
     @name = name
+    @departments = []
+  end
+
+  def add_department(dep_name)
+    @departments << dep_name
+  end
+
+  def deps_with_multiple
+    deps_with_multiple_array = []
+    @departments.each do |department|
+      if department.employees.count > 1
+        deps_with_multiple_array << department
+      else
+      end
+    end
+    # binding.pry
+    deps_with_multiple_array
   end
 end
--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -6,14 +6,54 @@ require 'pry'
 RSpec.describe Division do
   describe '#iteration 3' do
     it 'exists' do
-      bottling = Division.new("Bottling")
+      assembly = Division.new("Assembly")
 
-      expect(bottling).to be_a(Division)
+      expect(assembly).to be_a(Division)
     end
 
     it 'can read the name' do
-      bottling = Division.new("Bottling")
-      expect(bottling.name).to eq("Bottling")
-    end 
+      assembly = Division.new("Assembly")
+      expect(assembly.name).to eq("Assembly")
+    end
+
+    it 'has array to hold its departments' do
+      assembly = Division.new("Assembly")
+      bottling = Department.new("Bottling")
+      quality_control = Department.new("Quality Control")
+
+      expect(assembly.departments).to eq([])
+    end
+
+    it 'can add to the array of departments' do
+      assembly = Division.new("Assembly")
+      bottling = Department.new("Bottling")
+      quality_control = Department.new("Quality Control")
+
+      expect(assembly.departments).to eq([])
+      assembly.add_department(bottling)
+      assembly.add_department(quality_control)
+      #
+      expect(assembly.departments).to eq([bottling, quality_control])
+    end
+
+    it 'can return any department with multiple employees' do
+      assembly = Division.new("Assembly")
+      bottling = Department.new("Bottling")
+      quality_control = Department.new("Quality Control")
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+      aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+      dani = Employee.new({name: "Dani Dani", age: "30", salary: "125000"})
+      luke = Employee.new({name: "Luke Pascale", age: "36", salary: "98000"})
+      assembly.add_department(bottling)
+      assembly.add_department(quality_control)
+      quality_control.hire(bobbi)
+      quality_control.hire(luke)
+      quality_control.hire(dani)
+      bottling.hire(aaron)
+
+      expect(quality_control.employees).to eq([bobbi, luke, dani])
+      expect(bottling.employees).to eq([aaron])
+      expect(assembly.deps_with_multiple).to eq([quality_control])
+    end
   end
 end
